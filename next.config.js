5b182aa975aedbf0a2d4a9cfb1f81bcf92df3593
Jmorinno1 <jmorinno@gmail.com>
2025-05-03 12:54:02 +0100

    Corregidos problemas de importación y configuración de rutas

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
